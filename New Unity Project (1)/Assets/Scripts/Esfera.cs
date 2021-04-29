5488e0abbc11f9f988f5c1191d0ffc2c6e9ece24
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-04-29 19:37:36 +0200

    Nueva Nave
    Naves, Columnas y Spawner.

---
+++ New Unity Project (1)/Assets/Scripts/Esfera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esfera : MonoBehaviour
+{
+    Vector3 Jak;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jak = transform.localPosition;
+        Jak.y += Input.GetAxis("Jump") * Time.deltaTime * 12;
+        Jak.x += Input.GetAxis("Horizontal") * Time.deltaTime * 12;
+        Jak.z += Input.GetAxis("Vertical") * Time.deltaTime * 12;
+        transform.localPosition = Jak;
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/SpawnColumna.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnColumna : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject enemigo;
+    float posicionx;
+    float posicionz;
+    public int n;
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Spawner()
+    {
+        for (float n = 0; n < 10; n++)
+        {
+            posicionx = Random.Range(-10, 10);
+            posicionz = Random.Range(-10, 10);
+            Instantiate(enemigo, new Vector3(posicionx, 4, posicionz), Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+}
