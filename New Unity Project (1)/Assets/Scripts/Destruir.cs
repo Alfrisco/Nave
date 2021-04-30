914717f0f49ecb4df5dbe361879d779e61daee8a
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-04-30 19:52:56 +0200

    Destruccion en colision
    Añadido al chocar la columna se destruya

---
+++ New Unity Project (1)/Assets/Scripts/Destruir.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruir : MonoBehaviour
+{
+    public GameObject Esfera;
+    private Esfera nave;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("objeto1 ha colisionado con objeto2");
+        GameObject objeto1 = GameObject.Find("Columna");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("objeto1 ha colisionado con objeto3");
+            Destroy(gameObject);
+        }
+
+    }
+}
