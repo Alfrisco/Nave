a3d93eaea9a717a8a80a039eeba469f31cfa41c3
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-05-05 19:50:43 +0200

    Botones de arranque
    Start añadido

---
+++ New Unity Project (1)/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartLevel : MonoBehaviour
+{
+    public void LevelButton(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void LevelButton(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+            SceneManager.LoadScene("SampleScene");
+    }
+}
