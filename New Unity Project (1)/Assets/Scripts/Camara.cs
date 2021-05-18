fb3adbab616f4f2a20cff515e3312a9a98706b95
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-05-18 20:21:21 +0200

    Control y camara

---
+++ New Unity Project (1)/Assets/Scripts/Camara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 _offset;
+
+    void Start()
+    {
+        _offset = new Vector3(0, 4, -10); // or whatever you need
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Player.transform.position + _offset;
+        transform.rotation = Player.transform.rotation;
+    }
+}
